279ae850e67209ba37c59ba506af39bc2d3784d5
Artem Matiushenko <artom.matyushenko@gmail.com>
2022-02-22 22:08:38 +0200

    Formatted code

--- server.go
+++ server.go
@@ -9,23 +9,21 @@ import (
 
 func handleRequest(rw http.ResponseWriter, r *http.Request) {
 
-  rw.Header().Set("Content-Type", "application/json")
-  currentDate := time.Now().Format(time.RFC3339)
-  
-  resp := make(map[string]string)
-  
-  resp["time"] = currentDate
+	rw.Header().Set("Content-Type", "application/json")
+	currentDate := time.Now().Format(time.RFC3339)
 
-  err := json.NewEncoder(rw).Encode(resp)
-  if err != nil {
-    log.Fatalf("Error happened in while encoding. Err: %s", err)
-  }
+	resp := make(map[string]string)
+
+	resp["time"] = currentDate
+
+	err := json.NewEncoder(rw).Encode(resp)
+	if err != nil {
+		log.Fatalf("Error happened in while encoding. Err: %s", err)
+	}
 }
-
 
 func main() {
 
-
-  http.HandleFunc("/time", handleRequest) 
-  http.ListenAndServe(":8795", nil)
+	http.HandleFunc("/time", handleRequest)
+	http.ListenAndServe(":8795", nil)
 }
