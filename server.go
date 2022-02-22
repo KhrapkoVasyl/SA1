b73fca777cb45a485de3fe7198112cb6bad0ac0c
VasylKhrapko <khrapko2002@gmail.com>
2022-02-22 21:37:50 +0200

    Added handler function which send current-time response in routing /time

--- server.go
+++ server.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 	"time"
 )
 
-func handleRequest(rw http.ResponseWriter, r *http.Request) {
-	currentDate := time.Now().Format(time.RFC3339)
-	rw.Write([]byte(currentDate))
-}
+
+
 
 func main() {
-	http.HandleFunc("/", handleRequest)
-	http.ListenAndServe(":8080", nil)
+
+
+
+  http.HandleFunc("/time", func (rw http.ResponseWriter, r *http.Request) {
+
+    rw.Header().Set("Content-Type", "application/json")
+    currentDate := time.Now().Format(time.RFC3339)
+    
+    resp := make(map[string]string)
+    resp["time"] = currentDate
+    err := json.NewEncoder(rw).Encode(resp)
+    if err != nil {
+      log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+    }
+  })
+  http.ListenAndServe(":8795", nil)
 }
