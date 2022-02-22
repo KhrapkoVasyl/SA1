8033ac78e082c7342e67bacb1e498f482afe227d
VasylKhrapko <khrapko2002@gmail.com>
2022-02-22 21:47:35 +0200

    refactoring: handler-function moved to a separate function

--- server.go
+++ server.go
@@ -7,24 +7,26 @@ import (
 	"time"
 )
 
+func handleRequest(rw http.ResponseWriter, r *http.Request) {
 
+  rw.Header().Set("Content-Type", "application/json")
+  currentDate := time.Now().Format(time.RFC3339)
+  
+  resp := make(map[string]string)
+  
+  resp["time"] = currentDate
+
+  err := json.NewEncoder(rw).Encode(resp)
+  if err != nil {
+    log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+  }
+}
 
 
 func main() {
 
 
 
-  http.HandleFunc("/time", func (rw http.ResponseWriter, r *http.Request) {
-
-    rw.Header().Set("Content-Type", "application/json")
-    currentDate := time.Now().Format(time.RFC3339)
-    
-    resp := make(map[string]string)
-    resp["time"] = currentDate
-    err := json.NewEncoder(rw).Encode(resp)
-    if err != nil {
-      log.Fatalf("Error happened in JSON marshal. Err: %s", err)
-    }
-  })
+  http.HandleFunc("/time", handleRequest) 
   http.ListenAndServe(":8795", nil)
 }
